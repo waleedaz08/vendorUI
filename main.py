7420ac35ed22e6c5df052d28b5c535697d58073d
Waleed Khalid <wakhalid08@gmail.com>
2025-09-05 15:11:04 +0200

    first commit python streamlit app

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import sqlite3
+import pandas as pd
+
+conn = sqlite3.connect(r"C:\Users\DELL\Documents\practice.db")
+
+df = pd.read_sql_query("SELECT * FROM vendor", conn)
+
+st.title("Vendor Transactions")
+
+st.dataframe(df)
+
